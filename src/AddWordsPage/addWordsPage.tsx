93bdc8a22a06abe90c9993880411256f570bd86f
kukla <kukla.1989@ukr.net>
2024-12-03 14:15:29 +0200

    add add-words-page

---
+++ src/AddWordsPage/addWordsPage.tsx
@@ -0,0 +1,10 @@
+const addWordsPage = () => {
+  return (
+    <div className="add-words-page card">
+      <div className="add-words-page__title">pls add words that you want to learn and press enter</div>
+      <input type="text" className="add-words-page__input" />
+    </div>
+  );
+};
+
+export default addWordsPage;
