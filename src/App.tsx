e7533e42105714edb32632dbbaabe54054aa1724
kukla <kukla.1989@ukr.net>
2024-12-19 11:54:49 +0200

    add vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.scss';
-import { Main } from './Main/Main';
+import { Main } from './components/Main/Main';
 
 export const App: React.FC = () => {
   return (
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import { App } from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
+import "./index.scss";
+import { App } from "./App";
+
+const root = createRoot(document.getElementById("root")!);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </React.StrictMode>
 );
