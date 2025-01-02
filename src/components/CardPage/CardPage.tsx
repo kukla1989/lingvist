eb76de3dc3c46151db2b4ef6f79936103524df14
kukla <kukla.1989@ukr.net>
2025-01-02 15:10:49 +0200

    add sentence to word

--- src/components/CardPage/CardPage.tsx
+++ src/components/CardPage/CardPage.tsx
@@ -1,12 +1,30 @@
-import RepeatRemain from '../WordProgress/WordProgress';
-import styles from './CardPage.module.scss';
+import { useState } from "react";
+import WordProgress from "../WordProgress/WordProgress";
+import styles from "./CardPage.module.scss";
+const sentence1 = "Where are you? – I am at";
+const sentence2 = '.';
+const word = "home";
 
 const CardPage = () => {
+  const [userWord, setUserWord] = useState("");
+  const onSetWord = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setUserWord(e.target.value);
+  console.log(userWord, "word");
+
   return (
     <div className={styles.cardPage}>
       <div className={styles.card}>
-        <RepeatRemain level={4}/>
-        <div className="info">cardPage</div>
+        <WordProgress level={4} />
+        <div className={styles.sentence}>
+          <span className={styles.senteceBegin}>{sentence1}</span>
+          <input
+            className={styles.input}
+            value={userWord}
+            onChange={onSetWord}
+            style={{ width: 8 * word.length + 30 + "px" }}
+          />
+          <span className={styles.senteceEnd}>{sentence2}</span>
+        </div>
       </div>
     </div>
   );
