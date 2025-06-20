c510eca17e91d57a548e321f34e2efba1d432d3e
kukla <kukla.1989@ukr.net>
2025-06-20 10:19:28 +0300

    delete main page, add learn more on hover for WordProgress

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import './App.scss';
-import { Main } from './components/Main/Main';
+import style from './App.module.scss';
+import AddWordsPage from "./components/AddWordsPage/addWordsPage";
+import CardPage from "./components/CardPage/CardPage";
 
 export const App: React.FC = () => {
   return (
-    <div className="starter">
-      <Main />
+    <div className={style.app}>
+      <AddWordsPage />
+      <CardPage />
     </div>
   );
 };
