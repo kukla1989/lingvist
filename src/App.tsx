3f5df733d1e11dc8f22a3929de9a0f5cabade01d
kukla <kukla.1989@ukr.net>
2025-06-21 09:44:00 +0300

    add sentence to cardpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import style from './App.module.scss';
-import AddWordsPage from "./components/AddWordsPage/addWordsPage";
+// import AddWordsPage from "./components/AddWordsPage/addWordsPage";
 import CardPage from "./components/CardPage/CardPage";
 
 export const App: React.FC = () => {
   return (
     <div className={style.app}>
-      <AddWordsPage />
+      {/* <AddWordsPage /> */}
       <CardPage />
     </div>
   );
