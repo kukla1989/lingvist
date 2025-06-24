7533562eb8e87e54a1121bd07a70b7478ff16d6d
kukla <kukla.1989@ukr.net>
2025-06-24 08:48:35 +0300

    set up react router

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,22 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 import "./index.scss";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import CardPage from "./components/CardPage/CardPage";
+import AddWordsPage from "./components/AddWordsPage/addWordsPage";
 import { App } from "./App";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<CardPage />} />
+          <Route path="/home" element={<CardPage />} />
+          <Route path="/add-words" element={<AddWordsPage />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
