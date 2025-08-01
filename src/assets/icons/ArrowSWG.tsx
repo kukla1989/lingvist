d8cc4694694942f337415ba0c558ac5eacf52009
kukla <kukla.1989@ukr.net>
2025-08-01 12:00:18 +0300

    implement sign up page layout and content

--- src/assets/icons/ArrowSWG.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-const SvgIcon: React.FC<React.SVGProps<SVGElement>> = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" className="arrow" viewBox="0 0 24 24">
-    <path d="M6 18.3V5.7c0-1.3 1.4-2.1 2.5-1.4l10.7 6.3c1.1.7 1.1 2.2 0 2.9L8.5 19.8c-1.1.6-2.5-.2-2.5-1.5"></path>
-  </svg>
-);
-
-export default SvgIcon;
--- src/components/SignupPage/SignupPage.tsx
+++ src/components/SignupPage/SignupPage.tsx
@@ -1,12 +1,21 @@
 import GoogleSVG from "../../assets/icons/GoogleSVG";
+import UkrainianFlagSVG from "../../assets/icons/UkrainianFlagSVG";
+import ArrowSVG from "../../assets/icons/ArrowSVG";
+import BritainFlagSVG from "../../assets/icons/BritainFlagSVG";
 import styles from "./SignupPage.module.scss";
 
 const SignupPage = () => {
   return (
     <div className={styles.signupPage}>
-      <div className={styles.top}>Реєстрація</div>
+      <h1 className={styles.top}>Реєстрація</h1>
 
-      <div className={styles.main}>
+      <div className={styles.language}>
+        <UkrainianFlagSVG />
+        <ArrowSVG />
+        <BritainFlagSVG />
+      </div>
+
+      <div className={styles.auth}>
         <div className={styles.signupGoogle}>
           <GoogleSVG />
           Зареєструватися через Google
@@ -14,7 +23,7 @@ const SignupPage = () => {
 
         <div className={styles.agreement}>
           Реєструючись, ви погоджуєтеся з
-          <span className={styles.underline}>Умовами надання послуг</span>i
+          <span className={styles.underline}>Умовами надання послуг</span> {" "}i{" "}
           <span className={styles.underline}>Політикою Конфіденційності Lingvist.</span>
         </div>
       </div>
