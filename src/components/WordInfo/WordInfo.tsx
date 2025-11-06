4ce206039a405d75b2ee3b6e551316361175d0bd
kukla <kukla.1989@ukr.net>
2025-11-06 20:06:53 +0200

    add show more button; add group label

--- src/components/WordInfo/WordInfo.tsx
+++ src/components/WordInfo/WordInfo.tsx
@@ -1,13 +1,21 @@
 import styles from "./WordInfo.module.scss";
-import { WordType } from "../../assets/types.tsx";
+import { TranslationEntry, WordType } from "../../assets/types.tsx";
 import { v4 as uuidv4 } from "uuid";
-import { darkStyle, removeBraces } from "../../_utils/helpers.ts";
+import { removeBraces } from "../../_utils/helpers.ts";
 import DividerLine from "../DividerLine/DividerLine.tsx";
+import { useEffect, useState } from "react";
 
-function WordInfo({ wordInfo, searchWord }: {
+interface WordInfoProps {
   wordInfo: WordType;
   searchWord: (word: string) => void
-}) {
+}
+
+interface ShowMoreType {
+  [key: string]: boolean;
+}
+
+function WordInfo({ wordInfo, searchWord }: WordInfoProps) {
+  const [showMore, setShowMore] = useState<ShowMoreType>({});
   const {
     word,
     pronunciation,
@@ -18,7 +26,21 @@ function WordInfo({ wordInfo, searchWord }: {
     similarWords,
   } = wordInfo;
 
-  console.log(wordInfo, 'wordInfo');
+  useEffect(() => {
+    if (!(wordInfo.notFound || wordInfo.misspelled)) {
+      const showMoreObj: ShowMoreType = {};
+
+      Object.entries(wordInfo.translations)
+        .forEach(([partOfSpeech, definitionsByPartOfSpeech]) => {
+          if (definitionsByPartOfSpeech.length > 2) {
+            showMoreObj[partOfSpeech] = false;
+          }
+        })
+      setShowMore(showMoreObj);
+    }
+  }, [wordInfo])
+
+
   return (
     <div className={styles.wordInfo}>
       {notFound ? (<div className={styles.notFound}>Not found</div>) : (
@@ -31,13 +53,14 @@ function WordInfo({ wordInfo, searchWord }: {
             </div>
 
             {similarWords?.map(similarWord => (
-              <button className={darkStyle('similarWord', styles)}
+              <button className={styles.similarWord}
 
                       key={uuidv4()}
                       onClick={() => searchWord(similarWord)}>
                 {similarWord}
               </button>
-            ))}</>) : (<>
+            ))}
+          </>) : (<>
             <div className={styles.top}>
               <div className={styles.word}>{word}</div>
 
@@ -47,28 +70,44 @@ function WordInfo({ wordInfo, searchWord }: {
             </div>
 
             <div className={styles.translations}>
-              {Object.entries(translations).map(([partOfSpeech, definitionsArr]) => (
+              {Object.entries(translations).map(([partOfSpeech, definitionsByPartOfSpeech]) => (
                 <div className={styles.table} key={uuidv4()}>
                   <div className={styles.partOfSpeech}>{partOfSpeech}</div>
 
                   <div className={styles.definitions}>
-                    {definitionsArr.map(definitionGroup => (
-                      <div key={uuidv4()} className={styles.definitionGroup}>
-                        {definitionGroup.map(definitionEntry => (
-                          <div key={uuidv4()} className={styles.definitionItem}>
-                            <div
-                              className={styles.definition}>{removeBraces(definitionEntry.definition)}</div>
+                    {definitionsByPartOfSpeech.slice(0, (showMore[partOfSpeech] ? 20 : 2))
+                      .map((definitionGroup, ind) => (
+                        <div className={styles.definitionSection}
+                             key={uuidv4()}>
+                          <p className={styles.ind}>{ind + 1}</p>
 
-                            {definitionEntry.example && (
-                              <div
-                                className={styles.example}>{removeBraces(definitionEntry.example)}</div>
-                            )}
-                            <DividerLine />
+                          <div key={uuidv4()}
+                               className={styles.definitionGroup}>
+                            {definitionGroup.map(definitionEntry => (
+                              <div key={uuidv4()}
+                                   className={styles.definitionItem}>
+                                <div
+                                  className={styles.definition}>{removeBraces(definitionEntry.definition)}</div>
+
+                                {definitionEntry.example && (
+                                  <div
+                                    className={styles.example}>{removeBraces(definitionEntry.example)}</div>
+                                )}
+                                <DividerLine />
+                              </div>
+                            ))}
                           </div>
-                        ))}
-                      </div>
-                    ))}
+                        </div>
+                      ))}
                   </div>
+                  {(showMore[partOfSpeech] !== undefined) && (
+                    <button className={styles.showMore}
+                      onClick={() => setShowMore({ ...showMore, [partOfSpeech]: !showMore[partOfSpeech] })}
+                    >
+                      {showMore[partOfSpeech]
+                        ? 'hide'
+                        : ('show more ' + calculateRemainingShowMore(definitionsByPartOfSpeech.slice(2)))}
+                    </button>)}
                 </div>
               ))}
             </div>
@@ -80,3 +119,7 @@ function WordInfo({ wordInfo, searchWord }: {
 }
 
 export default WordInfo;
+
+function calculateRemainingShowMore(strs: TranslationEntry[][]) {
+  return strs.reduce((length, el) => length + el.length, 0);
+}
